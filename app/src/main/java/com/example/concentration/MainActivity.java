7579b357e5cebc405851af1b83ac7eaa667d74c6
PureDefender <sydney.ho35@gmail.com>
2019-11-21 14:39:28 -0800

    renamed images (they can/will be replaced later)

--- app/src/main/java/com/example/concentration/MainActivity.java
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.concentration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView player_score;
+
+    ImageView iv_11, iv_12, iv_13, iv_14, iv_21, iv_22, iv_23, iv_24, iv_31, iv_32, iv_33, iv_34, iv_41, iv_42, iv_43, iv_44;
+
+    Integer[] cardsArray = {101, 102, 103, 104, 105, 106, 201, 202, 203, 204, 205, 206, 301, 302, 303, 304, 305, 306};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player_score = (TextView) findViewById(R.id.player_score);
+        iv_11 = (ImageView) findViewById(R.id.iv_11);
+        iv_12 = (ImageView) findViewById(R.id.iv_12);
+        iv_13 = (ImageView) findViewById(R.id.iv_13);
+        iv_14 = (ImageView) findViewById(R.id.iv_14);
+
+        iv_21 = (ImageView) findViewById(R.id.iv_21);
+        iv_22 = (ImageView) findViewById(R.id.iv_22);
+        iv_23 = (ImageView) findViewById(R.id.iv_23);
+        iv_24 = (ImageView) findViewById(R.id.iv_24);
+
+        iv_31 = (ImageView) findViewById(R.id.iv_31);
+        iv_32 = (ImageView) findViewById(R.id.iv_32);
+        iv_33 = (ImageView) findViewById(R.id.iv_33);
+        iv_44 = (ImageView) findViewById(R.id.iv_34);
+
+        iv_41 = (ImageView) findViewById(R.id.iv_41);
+        iv_42 = (ImageView) findViewById(R.id.iv_42);
+        iv_43 = (ImageView) findViewById(R.id.iv_43);
+        iv_44 = (ImageView) findViewById(R.id.iv_44);
     }
 }
