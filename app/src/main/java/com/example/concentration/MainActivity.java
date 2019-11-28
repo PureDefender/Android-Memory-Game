e812231ad31d5095e9057be049bc6807672679bf
PureDefender <sydney.ho35@gmail.com>
2019-11-28 11:42:21 -0800

    current working v1.2, implemented landscape portrait

--- app/src/main/java/com/example/concentration/MainActivity.java
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -9,13 +9,19 @@ import java.util.TimerTask;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.Gravity;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -34,9 +40,13 @@ import androidx.annotation.RequiresApi;
 public class MainActivity extends Activity {
     private static int ROW_COUNT = -1;
     private static int COL_COUNT = -1;
+    private static int REMAINDER = -1;
+    public static boolean PORTRAIT_MODE = true;
+
     private Context context;
     private Drawable backImage;
-    private int [] [] cards;
+    private int[][] cards;
+
     private List<Drawable> images;
     private Card firstCard;
     private Card secondCard;
@@ -53,70 +63,92 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
 
-
         handler = new UpdateCardsHandler();
         loadImages();
         setContentView(R.layout.activity_main);
 
-        backImage =  getResources().getDrawable(R.drawable.yugi_card_back);
+        OrientationEventListener orientationListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                PORTRAIT_MODE = ((orientation < 100) || (orientation > 280));
+                if (PORTRAIT_MODE) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                } else {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+                Log.w("Orient", orientation + " PORTRAIT_MODE = " + PORTRAIT_MODE);
+            }
+        };
+
+        orientationListener.enable();
+        backImage = getResources().getDrawable(R.drawable.yugi_card_back);
 
         buttonListener = new ButtonListener();
 
-        mainTable = (TableLayout)findViewById(R.id.TableLayout03);
+        mainTable = findViewById(R.id.TableLayout03);
 
 
-        context  = mainTable.getContext();
+        context = mainTable.getContext();
 
-        Spinner s = (Spinner) findViewById(R.id.Spinner01);
+        Spinner s = findViewById(R.id.Spinner01);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
                 this, R.array.type, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
 
 
-        s.setOnItemSelectedListener(new OnItemSelectedListener(){
+        s.setOnItemSelectedListener(new OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(
                     android.widget.AdapterView<?> arg0,
-                    View arg1, int pos, long arg3){
+                    View arg1, int pos, long arg3) {
 
                 ((Spinner) findViewById(R.id.Spinner01)).setSelection(0);
 
-                int x,y;
+                int c, r;
 
                 switch (pos) {
                     case 1:
-                        x=2;y=2;
+                        c = 2;
+                        r = 2;
                         break;
                     case 2:
-                        x=2;y=3;
+                        c = 2;
+                        r = 3;
                         break;
                     case 3:
-                        x=2;y=4;
+                        c = 4;
+                        r = 2;
                         break;
                     case 4:
-                        x=2;y=5;
+                        c = 5;
+                        r = 2;
                         break;
                     case 5:
-                        x=3;y=4;
+                        c = 4;
+                        r = 3;
                         break;
                     case 6:
-                        x=2; y=7;
+                        c = 7;
+                        r = 2;
                         break;
                     case 7:
-                        x=4; y=4;
+                        c = 4;
+                        r = 4;
                         break;
                     case 8:
-                        x=2; y=9;
+                        c = 9;
+                        r = 2;
                         break;
                     case 9:
-                        x=4; y=5;
+                        c = 5;
+                        r = 4;
                         break;
                     default:
                         return;
                 }
-                newGame(x,y);
+                newGame(c, r);
 
             }
 
@@ -134,7 +166,7 @@ public class MainActivity extends Activity {
         ROW_COUNT = r;
         COL_COUNT = c;
 
-        cards = new int [COL_COUNT] [ROW_COUNT];
+        cards = new int[COL_COUNT][ROW_COUNT];
 
 
         mainTable.removeView(findViewById(R.id.TableRow01));
@@ -143,20 +175,39 @@ public class MainActivity extends Activity {
         TableRow tr = findViewById(R.id.TableRow03);
         tr.removeAllViews();
 
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int displayHeight = size.y;
+
+
         mainTable = new TableLayout(context);
+        mainTable.setMinimumHeight(displayHeight);
+
+        tr.setWeightSum(1);
+
+        mainTable.setWeightSum(ROW_COUNT+1);
+        mainTable.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
+//        TableLayout.LayoutParams outParams = new TableLayout.LayoutParams();
         tr.addView(mainTable);
+        tr.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
 
         for (int y = 0; y < ROW_COUNT; y++) {
-            mainTable.addView(createRow(y));
+
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            params.weight = 1;
+//            params.height = 0;
+//            params.bottomMargin = 10;
+            TableRow tempRow = createRow(y);
+            mainTable.addView(tempRow, params);
         }
 
-        firstCard=null;
+        firstCard = null;
         loadCards();
 
-        turns=0;
-        ((TextView)findViewById(R.id.tv1)).setText("Tries: "+turns);
-
-
+        turns = 0;
+        ((TextView) findViewById(R.id.tv1)).setText("Tries: " + turns);
     }
 
     private void loadImages() {
@@ -180,7 +231,7 @@ public class MainActivity extends Activity {
 
             Log.i("loadCards()","size=" + size);
 
-            ArrayList<Integer> list = new ArrayList<Integer>();
+            ArrayList<Integer> list = new ArrayList<>();
 
             for(int i=0;i<size;i++){
                 list.add(i);
@@ -209,22 +260,36 @@ public class MainActivity extends Activity {
 
     }
 
-    private TableRow createRow(int y){
+    /**
+     * This method converts dp unit to equivalent pixels, depending on device density.
+     *
+     * @param dp      A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent px equivalent to dp depending on device density
+     */
+    public static int convertDpToPixel(float dp, Context context) {
+        return (int) (dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    private TableRow createRow(int y) {
         TableRow row = new TableRow(context);
         row.setHorizontalGravity(Gravity.CENTER);
 
         for (int x = 0; x < COL_COUNT; x++) {
             View view = createImageButton(x, y);
-            row.addView(view);
-            view.setLayoutParams(new TableRow.LayoutParams(200, 290));
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            params.weight = 1;
+            params.height = convertDpToPixel(105, context);
+            params.width = convertDpToPixel(70, context);
+            row.addView(view, params);
         }
         return row;
     }
 
-    private View createImageButton(int x, int y){
+    private View createImageButton(int x, int y) {
         Button button = new Button(context);
         button.setBackgroundDrawable(backImage);
-        button.setId(100*x+y);
+        button.setId(100 * x + y);
         button.setOnClickListener(buttonListener);
         return button;
     }
@@ -235,33 +300,32 @@ public class MainActivity extends Activity {
         public void onClick(View v) {
 
             synchronized (lock) {
-                if(firstCard!=null && secondCard != null){
+                if (firstCard != null && secondCard != null) {
                     return;
                 }
                 int id = v.getId();
-                int x = id/100;
-                int y = id%100;
-                turnCard((Button)v,x,y);
+                int x = id / 100;
+                int y = id % 100;
+                turnCard((Button) v, x, y);
             }
 
         }
 
-        private void turnCard(Button button,int x, int y) {
+        private void turnCard(Button button, int x, int y) {
             button.setBackgroundDrawable(images.get(cards[x][y]));
 
-            if(firstCard==null){
-                firstCard = new Card(button,x,y);
-            }
-            else{
+            if (firstCard == null) {
+                firstCard = new Card(button, x, y);
+            } else {
 
-                if(firstCard.x == x && firstCard.y == y){
+                if (firstCard.x == x && firstCard.y == y) {
                     return; //the user pressed the same card
                 }
 
-                secondCard = new Card(button,x,y);
+                secondCard = new Card(button, x, y);
 
                 turns++;
-                ((TextView)findViewById(R.id.tv1)).setText("Tries: "+turns);
+                ((TextView) findViewById(R.id.tv1)).setText("Tries: " + turns);
 
 
                 TimerTask tt = new TimerTask() {
@@ -269,12 +333,11 @@ public class MainActivity extends Activity {
                     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                     @Override
                     public void run() {
-                        try{
+                        try {
                             synchronized (lock) {
                                 handler.sendEmptyMessage(0);
                             }
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             Log.e("E1", Objects.requireNonNull(e.getMessage()));
                         }
                     }
@@ -289,7 +352,7 @@ public class MainActivity extends Activity {
 
     }
 
-    class UpdateCardsHandler extends Handler{
+    class UpdateCardsHandler extends Handler {
 
         @Override
         public void handleMessage(Message msg) {
@@ -297,22 +360,18 @@ public class MainActivity extends Activity {
                 checkCards();
             }
         }
-        public void checkCards(){
-            if(cards[secondCard.x][secondCard.y] == cards[firstCard.x][firstCard.y]){
+
+        public void checkCards() {
+            if (cards[secondCard.x][secondCard.y] == cards[firstCard.x][firstCard.y]) {
                 firstCard.button.setVisibility(View.INVISIBLE);
                 secondCard.button.setVisibility(View.INVISIBLE);
-            }
-            else {
+            } else {
                 secondCard.button.setBackgroundDrawable(backImage);
                 firstCard.button.setBackgroundDrawable(backImage);
             }
 
-            firstCard=null;
-            secondCard =null;
+            firstCard = null;
+            secondCard = null;
         }
     }
-
-
-
-
 }
